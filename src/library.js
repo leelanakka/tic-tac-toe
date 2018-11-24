5686ab312b7fb939cb01baec7f5a418840290f49
lucky <44020019+luckyganesh@users.noreply.github.com>
2018-11-24 11:19:54 +0530

    introduced turn changer function
    it will change the players turn successiely and will take the input from
    successive users

--- src/library.js
+++ src/library.js
@@ -1,17 +1,33 @@
-const readline = require("readline-sync").question;
+const read = require("readline-sync").question;
 
 const startGame = function(){
   console.log(welcomeNote());
-  console.log(readPlayerNames());
+  let players = readPlayerNames();
+  console.log(turnChanger(players)); 
 }
 const welcomeNote = function(){
   return "welcome to TIC TAC TOE ";
 }
+
+const readline  = function(msg) {
+  return read(msg);
+}
+
 const readPlayerNames  = function() {
-  let player1 = readline("Please enter player1 name: ")
-  let player2 = readline("Please enter player2 name: ")
+  let player1 = read("Please enter player1 name: ")
+  let player2 = read("Please enter player2 name: ")
   return [player1,player2];
 }
+
+const turnChanger  = function(players){
+  let moves=[[],[]];
+  for(let index=0;index<9;index++){
+    let msg = "please enter "+players[index%2]+" turn : "; 
+    moves[index%2].push(read(msg));
+  };
+  return moves;
+}
+
 module.exports = { 
   welcomeNote,
   readPlayerNames,
